601b5a108e34ddfd89d570bc510ef13ca7c5b34e
AlexanderPro <illarionov.pro@gmail.com>
2020-11-17 19:33:43 +0300

    Added the Japanese language.

--- SmartSystemMenu/Settings/SmartSystemMenuSettings.cs
+++ SmartSystemMenu/Settings/SmartSystemMenuSettings.cs
@@ -171,6 +171,11 @@ namespace SmartSystemMenu.Settings
                 languageName = "cn";
             }
 
+            if (languageName == "" && Thread.CurrentThread.CurrentCulture.Name == "ja-JP")
+            {
+                languageName = "ja";
+            }
+
             if (languageName == "" && Thread.CurrentThread.CurrentCulture.Name == "ko-KR" || Thread.CurrentThread.CurrentCulture.Name == "ko-KP")
             {
                 languageName = "ko";
