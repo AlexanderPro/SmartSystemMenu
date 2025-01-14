3d385d17caec94b2ec43281613e8fb1c2da976f8
AlexanderPro <illarionov.pro@gmail.com>
2025-01-14 18:13:23 +0300

    Сhocolatey package v2.29.0

--- Package/Chocolatey/tools/chocolateyinstall.ps1
+++ Package/Chocolatey/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $packageName= 'smartsystemmenu'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/AlexanderPro/SmartSystemMenu/releases/download/v2.27.2/SmartSystemMenu_v2.27.2.zip'
+$url        = 'https://github.com/AlexanderPro/SmartSystemMenu/releases/download/v2.29.0/SmartSystemMenu_v2.29.0.zip'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -9,17 +9,8 @@ $packageArgs = @{
   fileType      = 'zip'
   url           = $url
   softwareName  = 'SmartSystemMenu*'
-  checksum      = '77e6383f27eea36841e1ec2a29d20fdf32332af5c0a031257909a781e6d3ebf8'
+  checksum      = '42b6c3378255b8893a3fe6301f610be48e96da920f0a9583bfb17dd8842ae70d'
   checksumType  = 'sha256'
 }
 
 Install-ChocolateyZipPackage @packageArgs
-
-
-
-
-
-
-
-
-
